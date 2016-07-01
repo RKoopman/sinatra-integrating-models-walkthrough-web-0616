29866b884c8871a598cf46f32c167a777ffbcdd5
RKoopman <raoul.koopman@gmail.com>
2016-07-01 10:30:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require_relative 'config/environment'
 require_relative 'models/textanalyzer.rb'
 
+
 class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
+
 
 post '/' do
-  text_from_user = params[:user_text]
+  @num_of_words = TextAnalyzer.new(params[:user_text]).count_of_words
   erb :results
 end
 
